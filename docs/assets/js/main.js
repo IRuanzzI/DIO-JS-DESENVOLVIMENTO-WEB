f7e57d1f0ca71e6ea00464c85c115d772481efc8
Ruan Pablo <rp824194@gmail.com>
2024-06-29 10:22:41 -0300

    search nav
    Adição de buscador, aumento no número de pokemons carregados em cada requisição para 50.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,7 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 const maxRecords = 150;
-const limit = 20;
+const limit = 50;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
@@ -20,6 +20,49 @@ function convertPokemonToLi(pokemon) {
             </div>
         </li>
     `
+}
+function filtrar() {
+    var input, filter, ol, li, name, number, types;
+
+    // Pegando os elementos em HTML
+    input = document.getElementById('buscador');
+    ol = document.getElementById('pokemonList');
+
+    // Filtro
+    filter = input.value.toUpperCase();
+
+    // Pegar todas as LI´s da lista
+    li = ol.getElementsByTagName('li');
+
+    // Percorrer todos os Li´s
+    for (let i = 0; i < li.length; i++) {
+        // Pegar a tag com a classe 'name', 'number' e 'types' do elemento percorrido
+        name = li[i].querySelector('.name');
+        number = li[i].querySelector('.number');
+        types = li[i].querySelectorAll('.type');
+
+        // Verificar se os elementos name, number e types existem
+        if (name && number && types.length > 0) {
+            // Pegar os textos dentro das tags
+            let nameValue = name.textContent || name.innerText;
+            let numberValue = number.textContent || number.innerText;
+            let typesValue = Array.from(types).map(type => type.textContent || type.innerText).join(' ');
+
+            // Verificar se o que o usuário digitou coincide com o nome, número ou tipo
+            if (nameValue.toUpperCase().indexOf(filter) > -1 ||
+                numberValue.toUpperCase().indexOf(filter) > -1 ||
+                typesValue.toUpperCase().indexOf(filter) > -1) {
+                // Valor coincide, mostrar o item
+                li[i].style.display = '';
+            } else {
+                // Valor não coincide, esconder o item
+                li[i].style.display = 'none';
+            }
+        } else {
+            // Se algum dos elementos não existir, esconder o item
+            li[i].style.display = 'none';
+        }
+    }
 }
 
 function loadPokemonItens(offset, limit) {
